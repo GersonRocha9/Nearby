68508739d8cbf37ab8404e794d6c27969b5c8209
Gerson Rocha <gersonrocha9@gmail.com>
2024-12-09 22:30:55 -0300

    add expo-font package and implement Rubik font in layout component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,28 @@
 import { Stack } from 'expo-router'
-import { colors } from '../styles/colors'
+
+import {
+  useFonts,
+  Rubik_600SemiBold as Rubik600SemiBold,
+  Rubik_400Regular as Rubik400Regular,
+  Rubik_500Medium as Rubik500Medium,
+  Rubik_700Bold as Rubik700Bold,
+} from '@expo-google-fonts/rubik'
+
+import { colors } from '@/styles/colors'
+import { Loading } from '@/components/loading'
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Rubik600SemiBold,
+    Rubik400Regular,
+    Rubik500Medium,
+    Rubik700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
     <Stack
       screenOptions={{
